8f1374144b6a6f176ea190d44fd4bf40f187be5f
razvizion <kolo111@gmail.com>
2014-03-08 18:31:13 +0100

    test

---
+++ Git Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Michał Kozak on 08.03.2014.
+//  Copyright (c) 2014 Raz Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ Git Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Michał Kozak on 08.03.2014.
+//  Copyright (c) 2014 Raz Labs. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
